7e46f77c03e960b2ba69dcc52d73e79b507b95d4
alex <alrosh7@gmail.com>
2020-07-21 21:10:36 +0100

    added getting scale in reconstruction

--- cmu_sparse_reconstuctor.py
+++ cmu_sparse_reconstuctor.py
@@ -4,8 +4,12 @@ import colmap
 from arrange_sessions import gen_query_txt
 import subprocess
 import glob
+from get_scale import calc_scale_COLMAP
+import sys
+import numpy as np
 
 # In order for this to work you have to transfer the images manually into the correct folders first
+original_images_path = sys.argv[1]
 
 #base mode paths
 base_db_path = "/home/alex/Mobile-Pose-Estimation-Pipeline-Prototype/colmap_data/data/base/database.db"
@@ -18,9 +22,11 @@ colmap.feature_extractor(base_db_path, base_images_dir)
 colmap.vocab_tree_matcher(base_db_path)
 colmap.mapper(base_db_path, base_images_dir, base_model_dir)
 
+scale = calc_scale_COLMAP(original_images_path, base_model_dir+"/0/images.bin")
+np.savetxt("/home/alex/Mobile-Pose-Estimation-Pipeline-Prototype/colmap_data/data/scale.txt", [scale])
+
 query_images_dir = "/home/alex/Mobile-Pose-Estimation-Pipeline-Prototype/colmap_data/data/live/images/"
 gen_query_txt(query_images_dir, base_images_no)
-
 
 # live mode paths
 live_db_path = "/home/alex/Mobile-Pose-Estimation-Pipeline-Prototype/colmap_data/data/live/database.db"
@@ -32,7 +38,7 @@ subprocess.run(["cp", base_db_path, live_db_path])
 
 colmap.feature_extractor(live_db_path, live_images_dir, live_query_image_list_file, query=True)
 colmap.vocab_tree_matcher(live_db_path, live_query_image_list_file)
-colmap.image_registrator(live_db_path, base_model_dir, live_model_dir)
+colmap.image_registrator(live_db_path, base_model_dir+"/0", live_model_dir)
 
 query_images_dir = "/home/alex/Mobile-Pose-Estimation-Pipeline-Prototype/colmap_data/data/gt/images/"
 gen_query_txt(query_images_dir)
