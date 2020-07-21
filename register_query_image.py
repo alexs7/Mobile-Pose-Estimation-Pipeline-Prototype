19f477c6766e8c5af8b9ce90e9754d8e10522b29
alex <alrosh7@gmail.com>
2020-07-21 17:58:58 +0100

    now passing commandline args

--- register_query_image.py
+++ register_query_image.py
@@ -1,7 +1,14 @@
 from image_registrator import register_image
+import sys
 
-register_image("colmap_data/data/database.db",
-               "colmap_data/data/current_query_image",
-               "colmap_data/data/query_name.txt",
-               "colmap_data/data/model/0",
-               "colmap_data/data/new_model")
+db_path = sys.argv[1] #"colmap_data/data/database.db"
+query_images_dir = sys.argv[2] #"colmap_data/data/current_query_image"
+query_images_list_file = sys.argv[3] #"colmap_data/data/query_name.txt"
+current_model = sys.argv[4] #"colmap_data/data/model/0"
+output_model = sys.argv[5] #"colmap_data/data/new_model"
+
+register_image(db_path,
+               query_images_dir,
+               query_images_list_file,
+               current_model,
+               output_model)
