aa379518c321892f02873be67bbf25a04d957bec
alex <alrosh7@gmail.com>
2020-07-22 19:08:48 +0100

    added changes to accomodate for Coop data/arcore

--- cmu_sparse_reconstuctor.py
+++ cmu_sparse_reconstuctor.py
@@ -4,17 +4,18 @@ import colmap
 from arrange_sessions import gen_query_txt
 import subprocess
 import glob
-from get_scale import calc_scale_COLMAP
+from get_scale import calc_scale_COLMAP, calc_scale_COLMAP_ARCORE
 import sys
 import numpy as np
 
 # In order for this to work you have to transfer the images manually into the correct folders first
-slide = sys.argv[1] # i.e 'slide14'
+path = sys.argv[1] # i.e /home/alex/fullpipeline/colmap_data/CMU_data/slice4/
+arcore = sys.argv[2] == '1'
 
 #base mode paths
-base_db_path = "/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/base/database.db"
-base_images_dir = "/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/base/images"
-base_model_dir = "/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/base/model"
+base_db_path = path+"base/database.db"
+base_images_dir = path+"base/images"
+base_model_dir = path+"base/model"
 
 base_images_no = len(glob.glob1(base_images_dir,"*.jpg"))
 
@@ -22,17 +23,21 @@ colmap.feature_extractor(base_db_path, base_images_dir)
 colmap.vocab_tree_matcher(base_db_path)
 colmap.mapper(base_db_path, base_images_dir, base_model_dir)
 
-scale = calc_scale_COLMAP(slide, base_model_dir+"/0/images.bin")
-np.savetxt("/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/scale.txt", [scale])
+if(arcore):
+    scale = calc_scale_COLMAP_ARCORE("/home/alex/Mobile-Pose-Estimation-Pipeline-Prototype/colmap_data/local_datasets/Coop/all_sessions/base_reference_data/", base_model_dir+"/0/images.bin")
+else:
+    scale = calc_scale_COLMAP(path, base_model_dir+"/0/images.bin")
 
-query_images_dir = "/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/live/images/"
+np.savetxt(path+"scale.txt", [scale])
+
+query_images_dir = path+"live/images/"
 gen_query_txt(query_images_dir, base_images_no)
 
 # live mode paths
-live_db_path = "/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/live/database.db"
-live_images_dir = "/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/live/images"
-live_model_dir = "/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/live/model"
-live_query_image_list_file = "/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/live/query_name.txt"
+live_db_path = path+"live/database.db"
+live_images_dir = path+"live/images"
+live_model_dir = path+"live/model"
+live_query_image_list_file = path+"live/query_name.txt"
 
 subprocess.run(["cp", base_db_path, live_db_path])
 
@@ -40,14 +45,14 @@ colmap.feature_extractor(live_db_path, live_images_dir, live_query_image_list_fi
 colmap.vocab_tree_matcher(live_db_path, live_query_image_list_file)
 colmap.image_registrator(live_db_path, base_model_dir+"/0", live_model_dir)
 
-query_images_dir = "/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/gt/images/"
+query_images_dir = path+"gt/images/"
 gen_query_txt(query_images_dir)
 
 # gt mode paths
-gt_db_path = "/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/gt/database.db"
-gt_images_dir = "/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/gt/images"
-gt_model_dir = "/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/gt/model"
-gt_query_image_list_file = "/home/alex/fullpipeline/colmap_data/CMU_data/"+slide+"/gt/query_name.txt"
+gt_db_path = path+"gt/database.db"
+gt_images_dir = path+"gt/images"
+gt_model_dir = path+"gt/model"
+gt_query_image_list_file = path+"gt/query_name.txt"
 
 subprocess.run(["cp", live_db_path, gt_db_path])
 
--- get_scale.py
+++ get_scale.py
@@ -3,41 +3,42 @@
 import glob
 import numpy as np
 import random
-from query_image import read_images_binary, get_image_camera_center_by_name
+from query_image import read_images_binary, get_image_camera_center_by_name, get_images_names
 
-def calc_scale_COLMAP_ARCORE(arcore_poses_path, colmap_model_from_arcore_images_path):
-    model_images = read_images_binary(colmap_model_from_arcore_images_path)
+def calc_scale_COLMAP_ARCORE(arcore_devices_poses_path, colmap_model_images_path):
+
+    model_images = read_images_binary(colmap_model_images_path)
     model_images_names = get_images_names(model_images)
-    frame_cam_centers = {}
-    for file in glob.glob(arcore_poses_path+"displayOrientedPose_*.txt"):
+    ar_core_cam_centers = {} #This in metric
+
+    for file in glob.glob(arcore_devices_poses_path+"displayOrientedPose_*.txt"):
         pose = np.loadtxt(file)
-        pose_t = np.array(pose[0:3, 3])
-        rot = np.array(pose[0:3, 0:3])
-        cam_center = -rot.transpose().dot(pose_t)
-        frame_cam_centers["frame_"+file.split("_")[-1].split(".")[0]+".jpg"] = pose_t
+        cam_center = np.array(pose[0:3, 3]) # remember in ARCore the matrices' t component is the camera center in the world
+        ar_core_cam_centers["frame_"+file.split("_")[-1].split(".")[0]+".jpg"] = cam_center
 
     scales = []
-    for i in range(1000):
+    for i in range(5000): #just to be safe
         random_images = random.sample(model_images_names, 2)
 
-        arcore_1_center = frame_cam_centers[random_images[0]]
-        arcore_2_center = frame_cam_centers[random_images[1]]
+        arcore_1_center = ar_core_cam_centers[random_images[0]]
+        arcore_2_center = ar_core_cam_centers[random_images[1]]
 
         model_cntr1 = get_image_camera_center_by_name(random_images[0], model_images)
         model_cntr2 = get_image_camera_center_by_name(random_images[1], model_images)
 
         model_cam_dst = np.linalg.norm(model_cntr1 - model_cntr2)
-        arcore_cam_dst = np.linalg.norm(arcore_1_center - arcore_2_center)
+        arcore_cam_dst = np.linalg.norm(arcore_1_center - arcore_2_center) #in meters
 
         scale = arcore_cam_dst / model_cam_dst
-
         scales.append(scale)
 
     return np.mean(scales)
 
-def calc_scale_COLMAP(slice, colmap_model_images_path):
+def calc_scale_COLMAP(path, colmap_model_images_path):
+    slice = path.split('/')[-2]
     original_images_path = "/home/alex/Datasets/Extended-CMU-Seasons/Extended-CMU-Seasons/"+slice+"/ground-truth-database-images-"+slice+".txt"
     model_images = read_images_binary(colmap_model_images_path)
+
     original_images = []
     with open(original_images_path) as f:
         original_images = f.readlines()
@@ -63,22 +64,9 @@ def calc_scale_COLMAP(slice, colmap_model_images_path):
         original_cntr2 = np.array(original_cam_centers[random_images[1]]).astype(np.float)
 
         model_cam_dst = np.linalg.norm(model_cntr1 - model_cntr2)
-        original_cam_dst = np.linalg.norm(original_cntr1 - original_cntr2)
+        original_cam_dst = np.linalg.norm(original_cntr1 - original_cntr2) #in meters
 
         scale = original_cam_dst / model_cam_dst
         scales.append(scale)
 
     return np.mean(scales)
-
-# print("Scale: ")
-# # # These will change manually
-# original_images_path = "/home/alex/Datasets/Extended-CMU-Seasons/Extended-CMU-Seasons/slice17/ground-truth-database-images-slice17.txt"
-# colmap_model_images_path = "/home/alex/Mobile-Pose-Estimation-Pipeline-Prototype/colmap_data/data/base/model/0/images.bin"
-#
-# print(calc_scale_COLMAP(original_images_path, colmap_model_images_path))
-
-# # they have to correspond
-# arcore_poses_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/all_data_and_models/coop_local/2020-06-17/morning/run_3/data_all/"
-# colmap_model_from_arcore_images_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/all_data_and_models/coop_local/2020-06-17/morning/run_3/reconstruction/0/images.bin"
-#
-# print(calc_scale_COLMAP_ARCORE(arcore_poses_path, colmap_model_from_arcore_images_path))
