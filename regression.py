2262480bbdc5724119c1e0459724c5910aa88f55
alex <alexandrosrotsidis@icloud.com>
2021-02-03 20:13:55 +0200

    NN working with 2 layers

--- regression.py
+++ regression.py
@@ -4,26 +4,54 @@ from tensorflow import estimator
 from tensorflow_core.python.keras import Sequential
 from tensorflow_core.python.keras.layers import Dense
 from tensorflow_core.python.keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow_core.python.keras.backend as K
 
-dataset = np.loadtxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/all_data_and_models/alfa_mega/points3D_sorted_descending_heatmap_per_image.txt")
+def soft_acc(y_true, y_pred):
+    return K.mean(K.equal(K.round(y_true), K.round(y_pred)))
 
-X = dataset[:,4:132] #sift vectors
-Y = dataset[:,3] #score
+idxs =  [*range(1,210)]
+training_data = np.empty([0, 129])
+
+for idx in idxs:
+    data = np.load("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/all_data_and_models/coop_local/ML_training_data/training_data_"+str(idx)+".npy")
+    training_data = np.r_[training_data, data]
+
+X = training_data[:, 0:128]
+y = training_data[:, 128]
+
+print("Data Preprocessing..")
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
+min_max_scaler = MinMaxScaler()
+y = min_max_scaler.fit_transform(y.reshape(-1, 1))
 
 # create model
 print("Creating model")
 model = Sequential()
 model.add(Dense(128, input_dim=128, kernel_initializer='normal', activation='relu'))
+model.add(Dense(32, input_dim=128, kernel_initializer='normal', activation='relu'))
 model.add(Dense(1, kernel_initializer='normal'))
 
 # Compile model
-model.compile(loss='mean_squared_error', optimizer='adam')
+model.compile(optimizer='rmsprop', loss='mse', metrics=[soft_acc])
 
 # train
 print("Training..")
-model.fit(X, Y, epochs=1500)
+model.fit(X, y, epochs=15, batch_size=16)
 
-print("Loading Model..")
+print("Evaluate Model..")
+test_data = np.load("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/all_data_and_models/coop_local/ML_training_data/training_data_last.npy")
+X = test_data[:, 0:128]
+y = test_data[:, 128]
+
+X = sc.fit_transform(X)
+y = min_max_scaler.fit_transform(y.reshape(-1, 1))
+
+model.evaluate(X,y)
+
 breakpoint()
 
 # evaluation
