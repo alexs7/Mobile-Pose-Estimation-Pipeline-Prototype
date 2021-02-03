0b846a0949b13d42c5f993d57e7605ce5d45948a
alex <alexandrosrotsidis@icloud.com>
2021-02-03 20:13:37 +0200

    minor changes in the localization script

--- single_image_localization.py
+++ single_image_localization.py
@@ -1,5 +1,15 @@
-# Remember to get the points average for the model first, get_points_3D_mean_desc_single_model.py
-#  and the scale python3 get_scale.py
+# Remember to get the points average for the model first,
+#  1) get_points_3D_mean_desc_single_model.py
+#  2) and the scale python3 get_scale.py
+
+# Sample output (with times and a model of 25k points)
+# Setting up..
+# Setting up took: 2.5194039344787598
+# Feature Extractor took: 1.2238779067993164 (COLMAP reports: 0.014 [minutes])
+# Feature Matching took: 1.1360161304473877
+# Solver took: 0.030557870864868164
+# Apply transformations took: 0.00560307502746582
+# Total time: 4.915458917617798
 
 import sys
 import time
@@ -16,17 +26,23 @@ def add_ones(x):
 
 K = np.loadtxt("/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/matrices/pixel_intrinsics_low_640_landscape.txt")
 query_frame_name = sys.argv[1] # same name as in query_name.txt but just the filename
-db_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/all_data_and_models/bedroom/database.db"
-points3D_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/all_data_and_models/bedroom/model/0/points3D.bin"
+db_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/database.db" #this doesn't need to be the original one it can be diff
+points3D_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/model/0/points3D.bin"
 query_images_dir = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/current_query_image"
-image_list_file = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/query_name.txt"
+image_list_file = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/query_name.txt" #the names here are relative to the path "query_images_dir/"
 descs_avg_path = "/Users/alex/Projects/EngDLocalProjects/LEGO/fullpipeline/colmap_data/data/descriptors_avg/avg_descs.npy"
 
+total_elapsed_time = 0
+start = time.time()
+print("Setting up..")
 db = COLMAPDatabase.connect(db_path)
 points3D = read_points3d_default(points3D_path)
 points3D_xyz = get_points3D_xyz(points3D) #new model maybe ?
+end = time.time()
+elapsed_time = end - start
+total_elapsed_time += elapsed_time
+print("Setting up took: " + str(elapsed_time))
 
-total_elapsed_time = 0
 # Step 1: Feature Extractor
 start = time.time()
 colmap.feature_extractor(db_path, query_images_dir, image_list_file)
@@ -55,7 +71,7 @@ print("Solver took: " + str(elapsed_time))
 # Step 4: Apply transformation to points
 start = time.time()
 arcore_pose = get_ARCore_pose_query_image()
-scale = 0.06324694325085609 #get_scale.py
+scale = 0.39034945701174184 #get_scale.py
 points3D_xyz = add_ones(points3D_xyz) # homogeneous
 points3DARCore = apply_transform(colmap_pose, arcore_pose, scale, points3D_xyz)
 end = time.time()
