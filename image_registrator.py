802ee8ed2c7417f535426193bf5251b2dd6cca62
alex <ar2056@bath.ac.uk>
2021-04-20 16:56:41 +0100

    image registrator - wip cuda problem

--- image_registrator.py
+++ image_registrator.py
@@ -1,6 +1,6 @@
 import colmap
 
-def register_image(db_path, query_images_dir, image_list_file, existing_model_dir, new_model_dir):
-    colmap.feature_extractor(db_path, query_images_dir, image_list_file)
+def register_image(db_path, query_images_dir, image_list_file, existing_model_dir, new_model_dir, queryParam=True):
+    colmap.feature_extractor(db_path, query_images_dir, image_list_file, query=queryParam)
     colmap.vocab_tree_matcher(db_path, image_list_file)
     colmap.image_registrator(db_path, existing_model_dir, new_model_dir)
--- register_query_image.py
+++ register_query_image.py
@@ -7,8 +7,17 @@ query_images_list_file = sys.argv[3] #"colmap_data/data/query_name.txt"
 current_model = sys.argv[4] #"colmap_data/data/model/0"
 output_model = sys.argv[5] #"colmap_data/data/new_model"
 
+# the database here that has to be used is the on from the sfm.
+# you could another db (an empty) but it makes no sense - no time to investigate
+# for ML purposes run this command
+
+# python3 register_query_image.py /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/database.db /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/test_images/2020-06-22 /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/images_list.txt /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/model /home/alex/fullpipeline/colmap_data/Coop_data/slice1/ML_data/new_model
+
 register_image(db_path,
                query_images_dir,
                query_images_list_file,
                current_model,
-               output_model)
+               output_model,
+               queryParam=True)
+
+
