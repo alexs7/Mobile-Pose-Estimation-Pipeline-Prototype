04d76c291b3005894d5a4004b4c7e2326c56e2fb
alexs7 <alexandrosrotsidis@icloud.com>
2022-05-05 10:22:45 +0300

    improved get scale

--- cyens_get_scale.py
+++ cyens_get_scale.py
@@ -12,8 +12,6 @@ from scipy.spatial.transform import Rotation as R
 def calc_scale_COLMAP_UNITY(unity_devices_poses_path, colmap_model_images_path):
 
     model_images = read_images_binary(colmap_model_images_path)
-    model_images_names = get_images_names(model_images)
-    model_images_names = model_images_names
     unity_cam_centers = {} #This in metric
 
     for file in glob.glob(os.path.join(unity_devices_poses_path,"local_pose_*.txt")): #or world_pose is the same
@@ -27,6 +25,7 @@ def calc_scale_COLMAP_UNITY(unity_devices_poses_path, colmap_model_images_path):
         unity_cam_centers["frame_"+file.split("_")[-1].split(".")[0]+".jpg"] = cam_center
 
     scales = []
+    model_images_names = list(unity_cam_centers.keys()) #we only need the frames localised from the phone (not the ones used to reconstruct the pointcloud)
     for i in range(5000): #just to be safe
         random_images = random.sample(model_images_names, 2)
 
@@ -35,6 +34,9 @@ def calc_scale_COLMAP_UNITY(unity_devices_poses_path, colmap_model_images_path):
 
         model_cntr1 = get_image_camera_center_by_name(random_images[0], model_images)
         model_cntr2 = get_image_camera_center_by_name(random_images[1], model_images)
+
+        if(model_cntr1.size==0 or model_cntr2.size==0): #this is to check if the unity frame has been localised or not (i.e is in the model)
+            continue
 
         model_cam_dst = np.linalg.norm(model_cntr1 - model_cntr2)
         unity_cam_dst = np.linalg.norm(unity_1_center - unity_2_center) #in meters
@@ -46,6 +48,11 @@ def calc_scale_COLMAP_UNITY(unity_devices_poses_path, colmap_model_images_path):
     print("Scale: " + str(scale))
     return scale
 
-unity_poses_path = sys.argv[1]
-colmap_poses_path = sys.argv[2]
-calc_scale_COLMAP_UNITY(unity_poses_path,colmap_poses_path)
+base_path = sys.argv[1]
+unity_poses_path = os.path.join(base_path,"scale_data")
+colmap_poses_path = os.path.join(base_path,"new_model/images.bin")
+scale_txt_path = os.path.join(base_path,"scale.txt")
+scale = calc_scale_COLMAP_UNITY(unity_poses_path,colmap_poses_path)
+
+with open(scale_txt_path, 'w') as f:
+    f.write(str(scale))
