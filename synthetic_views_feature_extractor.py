4761f478ea08c747ab7c1f712640eb59c630fc19
alex <alexandrosrotsidis@icloud.com>
2022-07-05 12:17:11 +0300

    added feature extractor script - debugging

--- synthetic_views_feature_extractor.py
+++ synthetic_views_feature_extractor.py
@@ -1,3 +1,4 @@
+import glob
 import sys
 import time
 from os.path import join
@@ -10,13 +11,24 @@ WIDTH = 1920
 HEIGHT = 1080
 
 base_path = sys.argv[1] # i.e. /Users/alex/Projects/CYENS/fullpipeline_cyens/cyens_data/Model 1 - Green Line Wall/
-image_path = os.path.join(base_path, "images/")
+images_path = os.path.join(base_path, "images/")
+depths_path = os.path.join(base_path, "depths/")
 image_features_path = os.path.join(base_path, "images_features/")
+no_images = len(glob.glob(os.path.join(images_path,"*.png")))
 
 if not os.path.exists(image_features_path):
     os.makedirs(image_features_path)
 
-for filename in os.scandir(image_path):
+for i in range(len(no_images)):
+    breakpoint()
+
+for filename in os.scandir(depths_path):
+    if filename.is_file():
+        print(filename.path)
+        breakpoint()
+        cv2.imread(filename.path, cv2.IMREAD_GRAYSCALE)
+
+for filename in os.scandir(images_path):
     if filename.is_file():
         print(filename.path)
         breakpoint()
