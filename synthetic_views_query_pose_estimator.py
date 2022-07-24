4f0b4a00ea3c4f805ca46bce2ee243e546953c21
alex <alexandrosrotsidis@icloud.com>
2022-07-24 19:07:36 +0300

    simplifying query pose estimator

--- synthetic_views_query_pose_estimator.py
+++ synthetic_views_query_pose_estimator.py
@@ -40,23 +40,14 @@ ratio_thresh = 0.7
 query_frame = sys.argv[2]
 synth_no = int(sys.argv[3]) #150, 12 ...
 
-print("Estimating a pose for image with name: " + query_frame)
-
 query_image_path = os.path.join(query_images_path, query_frame)
 query_image = cv2.imread(query_image_path)
 
-synth_image_path = os.path.join(synth_images_path, "original_{:05d}.png".format(synth_no))
-synth_image_color = cv2.imread(synth_image_path)
-synth_image = cv2.imread(synth_image_path, cv2.IMREAD_GRAYSCALE)
+synth_image_path = os.path.join(synth_images_path, "{:05d}.png".format(synth_no))
+synth_image = cv2.imread(synth_image_path)
 
 kps_query, descs_query = sift.detectAndCompute(query_image, None)
 kps_synth, descs_synth = sift.detectAndCompute(synth_image, None)
-
-keypoint_image = cv2.drawKeypoints(query_image, kps_query, 0, (0, 0, 255), flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
-keypoint_image_path = os.path.join(verifications_path, "query_keypoints_frame_{:06}.png".format(synth_no))
-cv2.imwrite(keypoint_image_path, keypoint_image)
-
-print("Matching between real and synth image db descs")
 
 matches = matcher.knnMatch(descs_query, descs_synth, k=2)
 # Need to draw only good matches, so create a mask
@@ -79,49 +70,95 @@ synth_query_matches_image = cv2.drawMatchesKnn(query_image, kps_query, synth_ima
 synth_query_matches_image_path = os.path.join(verifications_path, "synth_query_matches_image_{:06}.png".format(synth_no))
 cv2.imwrite(synth_query_matches_image_path, synth_query_matches_image)
 
-print("Matching between real and synth image (db features)..")
-
 database_features = db.get_feature_data(str(synth_no), row_length)
 descs_train = database_features[:, -128:].astype(np.float32) # last 128 elements (SIFT)
 
-temp_matches = matcher.knnMatch(descs_query, descs_train, k=2)
+breakpoint()
 
-good_matches = []
-for m, n in temp_matches:
-    if m.distance < ratio_thresh * n.distance:
-        good_matches.append(m)
+# Old code:
 
-# get good query indices
-good_query_keypoints = np.array(kps_query)[[good_match.queryIdx for good_match in good_matches]]
-# get good keypoints 2D
-keypoints_2D = np.array([good_query_keypoint.pt for good_query_keypoint in good_query_keypoints])
-# get good 3D points
-points_3D = database_features[[good_match.trainIdx for good_match in good_matches]][:, 2:5]
-# get good 2D points
-db_image_2D = database_features[[good_match.trainIdx for good_match in good_matches]][:, 0:2]
-
-for point in db_image_2D:
-    cv2.circle(synth_image_color, (int(point[0]), int(point[1])), 4, (0,255,0), -1)
-synth_image_db_keypoints_path = os.path.join(verifications_path, "synth_image_db_keypoints_path_{:06}.png".format(synth_no))
-cv2.imwrite(synth_image_db_keypoints_path, synth_image_color)
-
-print("good_matches_counter_between_images: " + str(good_matches_counter_between_images))
-print("good_matches_counter_between_image_and_database: " + str(len(good_matches)))
-
-K = np.loadtxt(sys.argv[4])
-
-_, rvec, tvec, _ = cv2.solvePnPRansac(points_3D, keypoints_2D, K, np.zeros((5, 1)),
-                                      iterationsCount = 3000, confidence = 0.99, flags = cv2.SOLVEPNP_P3P)
-
-rot_matrix = cv2.Rodrigues(rvec)[0] #second value is the jacobian
-est_pose_query = np.c_[rot_matrix, tvec]
-est_pose_query = np.r_[est_pose_query, [np.array([0, 0, 0, 1])]]
-
-print("Projecting points to verify..")
-verification_image_path = os.path.join(verifications_path, "verified_frame{:06}.png".format(synth_no))
-save_projected_points(points_3D, keypoints_2D, est_pose_query, K, query_image, verification_image_path)
-
-print("Done!...")
-end = time.time()
-elapsed_time = end - start
-print("Time taken (s): " + str(elapsed_time))
+# print("Estimating a pose for image with name: " + query_frame)
+#
+# query_image_path = os.path.join(query_images_path, query_frame)
+# query_image = cv2.imread(query_image_path)
+#
+# synth_image_path = os.path.join(synth_images_path, "original_{:05d}.png".format(synth_no))
+# synth_image_color = cv2.imread(synth_image_path)
+# synth_image = cv2.imread(synth_image_path, cv2.IMREAD_GRAYSCALE)
+#
+# kps_query, descs_query = sift.detectAndCompute(query_image, None)
+# kps_synth, descs_synth = sift.detectAndCompute(synth_image, None)
+#
+# keypoint_image = cv2.drawKeypoints(query_image, kps_query, 0, (0, 0, 255), flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+# keypoint_image_path = os.path.join(verifications_path, "query_keypoints_frame_{:06}.png".format(synth_no))
+# cv2.imwrite(keypoint_image_path, keypoint_image)
+#
+# print("Matching between real and synth image db descs")
+#
+# matches = matcher.knnMatch(descs_query, descs_synth, k=2)
+# # Need to draw only good matches, so create a mask
+# matchesMask = [[0,0] for i in range(len(matches))]
+#
+# good_matches_counter_between_images = 0
+# for i,(m,n) in enumerate(matches):
+#     if m.distance < ratio_thresh * n.distance:
+#         good_matches_counter_between_images += 1
+#         matchesMask[i]=[1,0]
+#
+# draw_params = dict(matchColor = (0,255,0),
+#                    singlePointColor = (255,0,0),
+#                    matchesMask = matchesMask,
+#                    flags = cv2.DrawMatchesFlags_DEFAULT)
+#
+# synth_query_matches_image = cv2.drawMatchesKnn(query_image, kps_query, synth_image,
+#                                                 kps_synth, matches, None, **draw_params)
+#
+# synth_query_matches_image_path = os.path.join(verifications_path, "synth_query_matches_image_{:06}.png".format(synth_no))
+# cv2.imwrite(synth_query_matches_image_path, synth_query_matches_image)
+#
+# print("Matching between real and synth image (db features)..")
+#
+# database_features = db.get_feature_data(str(synth_no), row_length)
+# descs_train = database_features[:, -128:].astype(np.float32) # last 128 elements (SIFT)
+#
+# temp_matches = matcher.knnMatch(descs_query, descs_train, k=2)
+#
+# good_matches = []
+# for m, n in temp_matches:
+#     if m.distance < ratio_thresh * n.distance:
+#         good_matches.append(m)
+#
+# # get good query indices
+# good_query_keypoints = np.array(kps_query)[[good_match.queryIdx for good_match in good_matches]]
+# # get good keypoints 2D
+# keypoints_2D = np.array([good_query_keypoint.pt for good_query_keypoint in good_query_keypoints])
+# # get good 3D points
+# points_3D = database_features[[good_match.trainIdx for good_match in good_matches]][:, 2:5]
+# # get good 2D points
+# db_image_2D = database_features[[good_match.trainIdx for good_match in good_matches]][:, 0:2]
+#
+# for point in db_image_2D:
+#     cv2.circle(synth_image_color, (int(point[0]), int(point[1])), 4, (0,255,0), -1)
+# synth_image_db_keypoints_path = os.path.join(verifications_path, "synth_image_db_keypoints_path_{:06}.png".format(synth_no))
+# cv2.imwrite(synth_image_db_keypoints_path, synth_image_color)
+#
+# print("good_matches_counter_between_images: " + str(good_matches_counter_between_images))
+# print("good_matches_counter_between_image_and_database: " + str(len(good_matches)))
+#
+# K = np.loadtxt(sys.argv[4])
+#
+# _, rvec, tvec, _ = cv2.solvePnPRansac(points_3D, keypoints_2D, K, np.zeros((5, 1)),
+#                                       iterationsCount = 3000, confidence = 0.99, flags = cv2.SOLVEPNP_P3P)
+#
+# rot_matrix = cv2.Rodrigues(rvec)[0] #second value is the jacobian
+# est_pose_query = np.c_[rot_matrix, tvec]
+# est_pose_query = np.r_[est_pose_query, [np.array([0, 0, 0, 1])]]
+#
+# print("Projecting points to verify..")
+# verification_image_path = os.path.join(verifications_path, "verified_frame{:06}.png".format(synth_no))
+# save_projected_points(points_3D, keypoints_2D, est_pose_query, K, query_image, verification_image_path)
+#
+# print("Done!...")
+# end = time.time()
+# elapsed_time = end - start
+# print("Time taken (s): " + str(elapsed_time))
